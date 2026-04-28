b67c0185d0959c948657d53d079c11c118f8dd6b
Josue velasquez <josueingc1@gmail.com>
2026-04-28 17:22:25 -0500

    Implement logger configuration in logger.py
    Add a logger configuration function to log messages to a file.

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,15 @@
+import logging
 
+def configurar_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler("sistema_errores.log")
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(handler)
+
+    return logger
