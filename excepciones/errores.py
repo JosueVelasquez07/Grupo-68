c8d3ce7c5da809b12821ad104bb3afba41b59ff4
josue velasuquez <josueingc1@gmail.com>
2026-04-29 15:58:11 -0500

    excepciones

--- excepciones/errores.py
+++ excepciones/errores.py
@@ -1,1 +1,16 @@
+class ReservaError(Exception):
+    def __init__(self, mensaje, codigo_error):
+        super().__init__(mensaje)
+        self.codigo_error = codigo_error
 
+
+class ClienteError(Exception):
+    def __init__(self, mensaje, codigo_error):
+        super().__init__(mensaje)
+        self.codigo_error = codigo_error
+
+
+class ServicioError(Exception):
+    def __init__(self, mensaje, codigo_error):
+        super().__init__(mensaje)
+        self.codigo_error = codigo_error
