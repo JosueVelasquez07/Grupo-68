432ea50eab36d7563868d23f79a96b985f1ec3f8
yepesflorezadrianalucia-hub <yepesflorezadrianalucia@gmail.com>
2026-05-03 16:10:31 -0500

    Implement ReservaSala class with validations
    Add ReservaSala class with validation and cost calculation

--- modelos/servicios/sala.py
+++ modelos/servicios/sala.py
@@ -1,1 +1,20 @@
+from .base import entidad
+from excepciones.errores import ReservaError
 
+class ReservaSala(entidad):
+  def _init_(self, nombre, precio_base, horas):
+    super()._init_(nombre)
+
+# Validacion del precio
+if precio_base <= 0:
+  raise ReservaError("El precio base debe ser mayor  0")
+
+# Validacion de horas
+if horas <=0:
+  raise ReservaError("Las horas deben ser mayores a 0")
+
+self.precio_base = precio_base
+self.horas = horas 
+
+def calcular_costo(self):
+  return sefl.precio_base * self.horas
