8672359b4ec4cb0c3e7519bcab02a3bfce489843
yepesflorezadrianalucia-hub <yepesflorezadrianalucia@gmail.com>
2026-05-03 15:56:57 -0500

    Add AlquilerEquipo class with validation and cost calculation

--- modelos/servicios/equipo.py
+++ modelos/servicios/equipo.py
@@ -1,1 +1,20 @@
+from .base import entidad
+from excepciones.errores import ReservaError
 
+class AlquilerEquipo(entidad):
+  def _init_(self, nombre, precio_base, dias):
+    super(). _init_(nombre)
+
+    # Validacion del Precio
+     if precio_base <=0:
+       raise ReservaError("El precio base debe ser mayor a 0")
+
+    # Validacion de dias 
+    if dias <=0:
+      raise ReservaError("Los dias deben ser mayores a 0")
+
+    self.precio_base = precio_base
+    self.dias = dias
+
+def calcular_costo(self):
+  return self.precio_base * self.dias
